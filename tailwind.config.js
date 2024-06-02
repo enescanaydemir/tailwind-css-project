58d07f936fa4bb608ffaa9607f2f48745a2c4816
Enes Can Aydemir <enescan_aydemir@hotmail.com>
2024-06-02 14:53:47 +0300

    Fonts and colors study

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,24 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+
+    container: {
+      center: true, //container classları her zaman ortalanması için yaptık. tek tek yazmak yerine burda tanımladık.
+    },
+
+    extend: {
+      fontFamily: {
+        gemunu: ['Gemunu Libre', 'sans-serif'],
+        open: ['Open Sans', 'sans-serif'],
+      },
+
+      colors: {
+        'gega-red': '#BC1A45', //renkleri ekledik ve bu renklere key isimleri verdik. bu keyleri class olarak index.html içerisinde kullandık
+        'gega-melon': '#FFD369',
+        'gega-grey': '#DDDDDD',
+        'gega-white': '#F7F7F7',
+      },
+    },
   },
   plugins: [],
 }
